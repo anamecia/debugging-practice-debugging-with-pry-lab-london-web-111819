5b5435a5719c3a35a2d356f9009d97738b5822eb
Ana Robalo <anameciarobalo@gmail.com>
2019-11-14 15:41:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -10,10 +10,16 @@ end
 
 def turtle_traits(turtles)
 
+  #turtles.map do |turtle|
+  #  turtle[:traits].each do |trait|
+  #    trait
+      
+  turtles_traits = []
   turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
+    if turtle[:trait].class == Array
+      turtles_traits << turtle[:trait]
+    else
+      turtles_traits << [turtle[:trait]]
     end
-   
   end
 end
